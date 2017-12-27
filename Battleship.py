83f357f46696ba7d5363ef4bff15f7ba7ba41612
WinterFive <piealacode@gmail.com>
2017-12-27 20:35:40 +0000

    Added playGame & setUpGame methods

--- Battleship.py
+++ Battleship.py
@@ -8,9 +8,24 @@ class Battleship:
     # make a console object
     myConsole = Console()
     
+    # create gameboards for user and computer
     userBoard = Gameboard()
     computerBoard = Gameboard()
     
+    def __main__(self):
+        myBattleship = Battleship()
+        myBattleship.beginGame(self)
+        
+    def beginGame(self):
+        while myConsole.playAgain is True:
+           setUpGame()
+           playGame()
+           
+    def setUpGame():
+        # TODO
+        
+    def playGame():
+        # TODO
     
 if __name__ == "__main__":
     # execute only if run as a script
